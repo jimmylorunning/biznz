e63abb4f89c281eabac54bb3503630a47d750898
Jimmy <jimmylorunning@gmail.com>
2015-02-06 21:54:26 -0500

    Initial commit -- forgot to commit earlier, already on step after generating serializer for contact

---
+++ config/application.rb
@@ -0,0 +1,29 @@
+require File.expand_path('../boot', __FILE__)
+
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
+require File.expand_path("../boot", __FILE__)
+require "rails/all"
+# require "rails/test_unit/railtie"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Api
+  class Application < Rails::Application
+#    config.active_record.raise_in_transactional_callbacks = true
+    config.middleware.use Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get, :post, :put, :delete, :options]
+      end
+    end
+  end
+end
